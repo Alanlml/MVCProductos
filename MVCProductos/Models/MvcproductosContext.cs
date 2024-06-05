08997d346ba5c3eb1e7e976e6703cd388598f60f
Alan <Alan@DESKTOP-6GOF9M0>
2024-06-05 00:32:30 -0300

    Agregar archivos de proyecto.

---
+++ MVCProductos/Models/MvcproductosContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCProductos.Models;
+
+public partial class MvcproductosContext : DbContext
+{
+    public MvcproductosContext()
+    {
+    }
+
+    public MvcproductosContext(DbContextOptions<MvcproductosContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("TuCadenaDeConexion");
+        }
+    }
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+    //      => optionsBuilder.UseSqlServer("server=(localdb)\\AlanCarvajal; database=MVCProductos; integrated security=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC2707CD3C22");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Marca)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Precio).HasColumnType("decimal(20, 0)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
